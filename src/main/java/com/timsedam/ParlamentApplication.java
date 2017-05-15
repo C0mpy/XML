f737abab4c2708a76e125ba77ce6d865b0f360a1
C0mpy <n.zunic@yahoo.com>
2017-05-16 00:05:00 +0200

    initial commit

---
+++ src/main/java/com/timsedam/ParlamentApplication.java
@@ -0,0 +1,13 @@
+package com.timsedam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParlamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParlamentApplication.class, args);
+	}
+
+}
